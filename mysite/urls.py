653a019fa404538f85f2d8d6a4f8c1788cc334bf
Chris Kyrouac <chriskyrouac@gmail.com>
2013-11-15 21:50:10 -0500

    Completed part 3 of tutorial.

--- mysite/urls.py
+++ mysite/urls.py
@@ -13,8 +13,8 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    url(r'^admin/', include(admin.site.urls, namespace="polls")),
-    url(r'^polls/', include('polls.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^polls/', include('polls.urls', namespace="polls")),
 )
 
 handler404 = 'mysite.views.handle404'
